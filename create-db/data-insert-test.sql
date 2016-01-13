e45e49e1d36a00472cbb22d45f19820984a82049
estefi-capece <eycapece@gmail.com>
2016-01-13 11:07:19 -0300

    Arreglé los nombres de los archivos y carpetas

---
+++ create-db/data-insert-test.sql
@@ -0,0 +1,5 @@
+INSERT INTO tuti.partidas VALUES (1,'vacio');
+INSERT INTO tuti.jugadores VALUES (1,'Meme',0);
+INSERT INTO tuti.categorias VALUES (1,1,'teoremas');
+INSERT INTO tuti.manos VALUES (1,1,'M','jugando');
+INSERT INTO tuti.jugadas VALUES (1,1,'Meme',1,'markov');
