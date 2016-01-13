c5a09537d6007a0841d55d562515e88312e73915
Emilio <Platzer>
2016-01-13 11:20:04 -0300

    mini emprolijamiento de la DB.

--- create-db/data-insert-test.sql
+++ create-db/data-insert-test.sql
@@ -1,5 +1,15 @@
-INSERT INTO tuti.partidas VALUES (1,'vacio');
+INSERT INTO tuti.partidas VALUES (1,'fin');
 INSERT INTO tuti.jugadores VALUES (1,'Meme',0);
+INSERT INTO tuti.jugadores VALUES (1,'Bart',0);
 INSERT INTO tuti.categorias VALUES (1,1,'teoremas');
-INSERT INTO tuti.manos VALUES (1,1,'M','jugando');
+INSERT INTO tuti.manos VALUES (1,1,'M','fin');
 INSERT INTO tuti.jugadas VALUES (1,1,'Meme',1,'markov');
+
+INSERT INTO tuti.partidas VALUES (2,'abierta');
+INSERT INTO tuti.jugadores VALUES 
+    (1,'Estefi',0),
+    (2,'Elba',0),
+    (3,'Guille',0),
+    (4,'Miru',0),
+    (5,'Emilio',0);
+
--- create-db/schema-tables.sql
+++ create-db/schema-tables.sql
@@ -1,90 +1,69 @@
 -- SCHEMA
 
 CREATE SCHEMA tuti
-  AUTHORIZATION tedede_php;
+  AUTHORIZATION tuti_owner;
 
-GRANT ALL ON SCHEMA tuti TO tedede_php;
-GRANT USAGE ON SCHEMA tuti TO tuti_users;
+GRANT ALL ON SCHEMA tuti TO tuti_owner;
+GRANT USAGE ON SCHEMA tuti TO tuti_node;
 
 -- TABLES
 
-CREATE TABLE tuti.partidas
-(
+CREATE TABLE tuti.partidas(
   partida integer NOT NULL,
   estado_partida character varying(50),
   CONSTRAINT partidas_pkey PRIMARY KEY (partida)
-)
-WITH (
-  OIDS=FALSE
 );
-ALTER TABLE tuti.partidas
-  OWNER TO tedede_php;
-GRANT ALL ON TABLE tuti.partidas TO tedede_php;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE tuti.partidas TO tuti_users;
+ALTER TABLE tuti.partidas OWNER TO tuti_owner;
+GRANT ALL ON TABLE tuti.partidas TO tuti_owner;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE tuti.partidas TO tuti_node;
 
 
-CREATE TABLE tuti.jugadores
-(
+CREATE TABLE tuti.jugadores(
   partida integer NOT NULL,
   jugador character varying(50),
   puntos integer NOT NULL,
   CONSTRAINT jugadores_pkey PRIMARY KEY (partida,jugador)
-)
-WITH (
-  OIDS=FALSE
 );
-ALTER TABLE tuti.jugadores
-  OWNER TO tedede_php;
-GRANT ALL ON TABLE tuti.jugadores TO tedede_php;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE tuti.jugadores TO tuti_users;
 
-CREATE TABLE tuti.categorias
-(
+ALTER TABLE tuti.jugadores OWNER TO tuti_owner;
+GRANT ALL ON TABLE tuti.jugadores TO tuti_owner;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE tuti.jugadores TO tuti_node;
+
+CREATE TABLE tuti.categorias(
   partida integer NOT NULL,
   categoria integer NOT NULL,
   cate_desc character varying(50),
   CONSTRAINT categorias_pkey PRIMARY KEY (partida,categoria)
-)
-WITH (
-  OIDS=FALSE
 );
 ALTER TABLE tuti.categorias
-  OWNER TO tedede_php;
-GRANT ALL ON TABLE tuti.categorias TO tedede_php;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE tuti.categorias TO tuti_users;
+  OWNER TO tuti_owner;
+GRANT ALL ON TABLE tuti.categorias TO tuti_owner;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE tuti.categorias TO tuti_node;
 
-CREATE TABLE tuti.manos
-(
+CREATE TABLE tuti.manos(
   partida integer NOT NULL,
   mano integer NOT NULL,
-  letra character varying(50),
+  letra character varying(1),
   estado_mano character varying(50),
   CONSTRAINT manos_pkey PRIMARY KEY (partida,mano)
-)
-WITH (
-  OIDS=FALSE
 );
 ALTER TABLE tuti.manos
-  OWNER TO tedede_php;
-GRANT ALL ON TABLE tuti.manos TO tedede_php;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE tuti.manos TO tuti_users;
+  OWNER TO tuti_owner;
+GRANT ALL ON TABLE tuti.manos TO tuti_owner;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE tuti.manos TO tuti_node;
 
-CREATE TABLE tuti.jugadas
-(
+CREATE TABLE tuti.jugadas(
   partida integer NOT NULL,
   mano integer NOT NULL,
   jugador character varying(50),
   categoria integer NOT NULL,
   palabra character varying(50),
   CONSTRAINT jugadas_pkey PRIMARY KEY (partida,mano,jugador,categoria,palabra)
-)
-WITH (
-  OIDS=FALSE
 );
 ALTER TABLE tuti.jugadas
-  OWNER TO tedede_php;
-GRANT ALL ON TABLE tuti.jugadas TO tedede_php;
-GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE tuti.jugadas TO tuti_users;
+  OWNER TO tuti_owner;
+GRANT ALL ON TABLE tuti.jugadas TO tuti_owner;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE tuti.jugadas TO tuti_node;
 
 
 
